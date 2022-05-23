ecc8adb6a952560c91b2a2701b9281d8fee7f83c
Gleb Nikolaev <glebn99@gmail.com>
2022-05-24 00:26:15 +0300

    funny js and some layout

--- js/index.js
+++ js/index.js
@@ -7,6 +7,33 @@ let profile_name = document.querySelector('.profile__h1');
 let comment = document.querySelector('.profile__p');
 let popap__container = document.querySelector('.popap__container');
 
+var like = document.querySelectorAll('.box_like');
+
+//function addLike() {
+//    like[0].classList.toggle('box_like_active');
+//   
+//}
+
+like[0].addEventListener('click', function () {
+    like[0].classList.toggle('box_like_active');
+});
+like[1].addEventListener('click', function () {
+    like[1].classList.toggle('box_like_active');
+});
+like[2].addEventListener('click', function () {
+    like[2].classList.toggle('box_like_active');
+});
+like[3].addEventListener('click', function () {
+    like[3].classList.toggle('box_like_active');
+});
+like[4].addEventListener('click', function () {
+    like[4].classList.toggle('box_like_active');
+});
+like[5].addEventListener('click', function () {
+    like[5].classList.toggle('box_like_active');
+});
+
+
 
 function ChangeProfile (event) {
     
@@ -17,7 +44,6 @@ function ChangeProfile (event) {
     popap.style.display = 'none';
    
 }
-  
 
 function ClosePopap () {
     popap.style.display = 'none';
@@ -30,3 +56,4 @@ function OpenPopap() {
 close_icon.addEventListener('click', ClosePopap);
 rectangle.addEventListener('click', OpenPopap);
 page.addEventListener('submit', ChangeProfile);
+
