9fae777c7878c3ed4e47399f60b1d760d41aed08
Gleb Nikolaev <glebn99@gmail.com>
2022-05-24 18:51:14 +0300

    last changes

--- js/index.js
+++ js/index.js
@@ -1,59 +1,28 @@
-let close_icon = document.querySelector('.close_icon');
+let closeButton = document.querySelector('.close_icon');
 let rectangle = document.querySelector('.rectangle');
-let popap = document.querySelector('.popap');
+let formElement = document.querySelector('.popap');
 let page = document.querySelector('.page');
 let inputs = document.querySelectorAll('input');
-let profile_name = document.querySelector('.profile__h1');
-let comment = document.querySelector('.profile__p');
-let popap__container = document.querySelector('.popap__container');
-
-var like = document.querySelectorAll('.box_like');
-
-//function addLike() {
-//    like[0].classList.toggle('box_like_active');
-//   
-//}
-
-like[0].addEventListener('click', function () {
-    like[0].classList.toggle('box_like_active');
-});
-like[1].addEventListener('click', function () {
-    like[1].classList.toggle('box_like_active');
-});
-like[2].addEventListener('click', function () {
-    like[2].classList.toggle('box_like_active');
-});
-like[3].addEventListener('click', function () {
-    like[3].classList.toggle('box_like_active');
-});
-like[4].addEventListener('click', function () {
-    like[4].classList.toggle('box_like_active');
-});
-like[5].addEventListener('click', function () {
-    like[5].classList.toggle('box_like_active');
-});
-
-
+let nameInput = document.querySelector('.profile__h1');
+let jobInput = document.querySelector('.profile__p');
 
 function ChangeProfile (event) {
-    
     event.preventDefault();
 
-    profile_name.textContent = inputs[0].value;
-    comment.textContent = inputs[1].value;
-    popap.style.display = 'none';
-   
+    nameInput.textContent = inputs[0].value;
+    jobInput.textContent = inputs[1].value;
+    formElement.style.display = 'none';
 }
 
 function ClosePopap () {
-    popap.style.display = 'none';
+    formElement.style.display = 'none';
 }
 
 function OpenPopap() {
-    popap.style.display = 'flex';
+    formElement.style.display = 'flex';
 }
 
-close_icon.addEventListener('click', ClosePopap);
+closeButton.addEventListener('click', ClosePopap);
 rectangle.addEventListener('click', OpenPopap);
 page.addEventListener('submit', ChangeProfile);
 
